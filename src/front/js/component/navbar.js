b660fbddb53b69889ceda4c14702c24639944025
JuanjoRuiz <juanjoserusa@gmail.com>
2022-12-27 10:10:46 +0000

    navbar created and styled

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -3,17 +3,33 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
+		<nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+	<a className="navbar-brand nav-title" href="#">HandleHome</a>
+	
+	
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse d-flex justify-content-center " id="navbarNav">
+      
+      <ul className="navbar-nav d-flex justify-content-evenly ">
+        <li className="nav-item ">
+          <a className="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Profesiones</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Mi perfil</a>
+        </li>
+      </ul>
+      
+    </div>
+	<div className="d-flex justify-content-end ">
+		<button type="button" className="btn btn-primary nav-bt">Login</button>
+	</div>
+  </div>
+</nav>
 	);
 };
