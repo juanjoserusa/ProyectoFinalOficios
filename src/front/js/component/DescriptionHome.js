983a13327e2bb918ea0cf801ea47da1782b575db
JuanjoRuiz <juanjoserusa@gmail.com>
2022-12-29 08:56:09 +0000

    Created footer and use all components in Home Screen

--- src/front/js/component/DescriptionHome.js
+++ src/front/js/component/DescriptionHome.js
@@ -3,17 +3,17 @@ import "../../styles/descriptionHome.css"
 
 const DescriptionHome = () => {
   return (
-    <section class=" mt-5 mb-5 py-4 py-md-11 description">
-      <div class="container-flex">
-        <div class="row d-flex justify-content-evenly">
-          <div class="col-3 col-md-3 cards me-2">
-            <div class="icon mb-3 ">
+    <section className=" mt-5 mb-5 py-4 py-md-11 description">
+      <div className="container-flex">
+        <div className="row d-flex justify-content-evenly">
+          <div className="col-3 col-md-3 cards me-2">
+            <div className="icon mb-3 ">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="30"
                 height="30"
                 fill="currentColor"
-                class="bi bi-tools"
+                className="bi bi-tools"
                 viewBox="0 0 16 16"
               >
                 <path d="M1 0 0 1l2.2 3.081a1 1 0 0 0 .815.419h.07a1 1 0 0 1 .708.293l2.675 2.675-2.617 2.654A3.003 3.003 0 0 0 0 13a3 3 0 1 0 5.878-.851l2.654-2.617.968.968-.305.914a1 1 0 0 0 .242 1.023l3.27 3.27a.997.997 0 0 0 1.414 0l1.586-1.586a.997.997 0 0 0 0-1.414l-3.27-3.27a1 1 0 0 0-1.023-.242L10.5 9.5l-.96-.96 2.68-2.643A3.005 3.005 0 0 0 16 3c0-.269-.035-.53-.102-.777l-2.14 2.141L12 4l-.364-1.757L13.777.102a3 3 0 0 0-3.675 3.68L7.462 6.46 4.793 3.793a1 1 0 0 1-.293-.707v-.071a1 1 0 0 0-.419-.814L1 0Zm9.646 10.646a.5.5 0 0 1 .708 0l2.914 2.915a.5.5 0 0 1-.707.707l-2.915-2.914a.5.5 0 0 1 0-.708ZM3 11l.471.242.529.026.287.445.445.287.026.529L5 13l-.242.471-.026.529-.445.287-.287.445-.529.026L3 15l-.471-.242L2 14.732l-.287-.445L1.268 14l-.026-.529L1 13l.242-.471.026-.529.445-.287.287-.445.529-.026L3 11Z" />
@@ -22,19 +22,19 @@ const DescriptionHome = () => {
 
             <h3>Oficios</h3>
 
-            <p class=" mb-6 mb-md-0">
+            <p className=" mb-6 mb-md-0">
               Busca al profesional adecuado para lo que necesitas, filtra por zonas y profesiones
             </p>
           </div>
           <div
-            class="col-3 col-md-3 cards me-2">
-            <div class="icon mb-3">
+            className="col-3 col-md-3 cards me-2">
+            <div className="icon mb-3">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="30"
                 height="30"
                 fill="currentColor"
-                class="bi bi-calendar-week"
+                className="bi bi-calendar-week"
                 viewBox="0 0 16 16"
               >
                 <path d="M11 6.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm-3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm-5 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1z" />
@@ -44,20 +44,20 @@ const DescriptionHome = () => {
 
             <h3>Calendario</h3>
 
-            <p class=" mb-6 mb-md-0">
+            <p className=" mb-6 mb-md-0">
               Ve el calendario actualizado de cada profesional para ajustar la fecha de tu reparacion o contratar por urgencia
             </p>
           </div>
           <div
-            class="col-3 col-md-3 cards me-2"
+            className="col-3 col-md-3 cards me-2"
           >
-            <div class="icon mb-3">
+            <div className="icon mb-3">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="30"
                 height="30"
                 fill="currentColor"
-                class="bi bi-pencil-square"
+                className="bi bi-pencil-square"
                 viewBox="0 0 16 16"
               >
                 <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
@@ -70,7 +70,7 @@ const DescriptionHome = () => {
 
             <h3>Comentarios</h3>
 
-            <p class="mb-0">
+            <p className="mb-0">
               Puedes ver comentarios de otros clientes antes de contratar al profesional que estas buscando
             </p>
           </div>
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -15,7 +15,6 @@ export const Home = () => {
 			<JumboCliente />
 			<DescriptionHome />
 			<JumboProfesional />
-			
 			<Cta />
 		</div>
 	);
