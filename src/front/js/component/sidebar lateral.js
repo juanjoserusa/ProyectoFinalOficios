35ca169189f1cd9de8a42a7e51a523108b680e0d
Dalyclnl <dalyarmijoa@yahoo.com>
2023-01-03 19:41:10 +0000

    detalles

---
+++ src/front/js/component/sidebar lateral.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+ function Sidebar() {
+    return (
+        <div className="Sidebar">
+          <ul className ="SidebarList">
+           <hi> Perfil <h1 
+   
+           <li  className="row"  onClick={() => {window.location.pathname=val.link;
+             }} "Calendario" > </li>
+        
+           <li  className="row"  onClick={() => {window.location.pathname=val.link;
+             }}  "Valoración" > </li>
+           <li className="row"  onClick={() => {window.location.pathname=val.link;
+             }} "Perfil" > </li>
+        
+         <li  className="row"  onClick={() => {window.location.pathname=val.link;
+             }} "Calendario" > </li>
+          </ul>    
+        <ul><hi> Status<h1  
+           
+            <li  className="row"  onClick={() => {window.location.pathname=val.link;
+               }} "Mensaje" > </li>
+  
+           <li  className="row"  onClick={() => {window.location.pathname=val.link;
+               }} "Propuesto Aceptado" > </li>
+          <li  className="row"  onClick={() => {window.location.pathname=val.link;
+               }} "Trabajs realizados" > </li>
+  
+           <li  className="row"  onClick={() => {window.location.pathname=val.link;
+             }} "Metodo de pago" > </li>
+  
+        </ul>
+     
+      </div>
+
+
+
+}
+export defaul Sidebar;
+  
+  
+  
+  
