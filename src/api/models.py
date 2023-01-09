5bbd0a3170b6a6456d8b7cdd79133b3cae5649c5
JuanjoRuiz <juanjoserusa@gmail.com>
2023-01-09 17:41:25 +0000

    cambios en el anuncio

--- src/api/models.py
+++ src/api/models.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 
+
 db = SQLAlchemy()
 
 class User(db.Model):
@@ -12,6 +13,8 @@ class User(db.Model):
     zipcode = db.Column(db.Integer, unique=False, nullable=False)
     userprofesional = db.Column(db.Boolean(), unique=False, nullable=True)
     userclient = db.Column(db.Boolean(), unique=False, nullable=True)
+    announce_id = db.Column(db.Integer, db.ForeignKey('UserAnnounceRelation.announce_id'), nullable=True, unique=True, )
+
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -20,5 +23,45 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
+            # do not serialize the password, its a security breach
+        }
+
+class UserAnnounceRelation(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("Announce.id"), nullable=False, unique=True)
+    announce_id = db.Column(db.Integer, db.ForeignKey("User.id"), nullable=False, unique=True)
+
+    def __repr__(self):
+        return f'<UserAnnounceRelation {self.user_id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "announce_id": self.announce_id,
+            # do not serialize the password, its a security breach
+        }
+
+
+
+class Announce(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    mail = db.Column(db.String(50), unique=False, nullable=False)
+    profession = db.Column(db.String(20), unique=False, nullable=False)
+    price= db.Column(db.Integer, unique=False, nullable=False)
+    zipcode = db.Column(db.Integer, unique=False, nullable=False)
+    description = db.Column(db.String(250), unique=False, nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("UserAnnounceRelation.user_id") , nullable=True, unique=True, )
+
+    def __repr__(self):
+        return f'<Announce {self.mail}>'
+
+    def serialize(self):
+        return {
+            "mail": self.mail,
+            "profession": self.profession,
+            "price": self.price,
+            "zipcode": self.zipcode,
+            "description": self.description,
             # do not serialize the password, its a security breach
         }
