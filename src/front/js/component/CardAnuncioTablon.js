868ff84e342a8dc690f9a9888a54d50023837f7b
JuanjoRuiz <juanjoserusa@gmail.com>
2023-01-10 19:51:20 +0000

    arreglos map

--- src/front/js/component/CardAnuncioTablon.js
+++ src/front/js/component/CardAnuncioTablon.js
@@ -2,18 +2,25 @@ import React from "react";
 import ModalProfesiones from "./ModalProfesiones";
 
 
-const CardAnuncioTablon = ({description, mail}) => {
+const CardAnuncioTablon = ({description, mail, profesion, codigoPostal, precio}) => {
   return (
 
-      <div className="col-2 card m-2" style={{ backgroundColor: "#ffface" }}>
-        <div className="card-body">
-          <h5 className="card-title">{mail}</h5>
-          <p className="card-text text-dark">
-            {description}
-          </p>
-          <ModalProfesiones />
-        </div>
-      </div>
+    <div className="col-2 card m-2" style={{ backgroundColor: "#ffface" }}>
+    <div className="card-body cardAnuncio">
+      <h5 className="card-title tituloCard">{profesion}</h5>
+      <p className="card-text text-dark anuncio">
+      {description}
+      </p>
+      
+  <ul className="datosCard">
+    <li>{mail}</li>
+    <li>{codigoPostal}</li>
+    <li>{precio}</li>
+  </ul>
+  
+      <ModalProfesiones />
+    </div>
+  </div>
 
   );
 };
