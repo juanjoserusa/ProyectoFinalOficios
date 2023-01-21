3ae8b2b2b1d545b6f5d959034bf74d1967821663
Dalyclnl <dalyarmijoa@yahoo.com>
2023-01-21 12:49:41 +0000

    en prueba envio mensj

--- src/api/routes.py
+++ src/api/routes.py
@@ -83,17 +83,14 @@ def recibirMensaje(id):
     return jsonify(all_mensaje)
 
 
-@api.route("/request_password", methods=["GET","POST"])
+
+@api.route("/request_password", methods=["GET" , "POST"])
 def request_password():
     email = request.json.get("email", None)
-    user = User.query.filter_by(email=email).first()
-    return jsonify({"mensaje": "link enviado!"}),200
-
-@api.route("/reset_password", methods=["GET","POST"])
-def reset_password():
-    password = request.json.get("password", None)
-    user = User.query.filter_by(email=email).first()
-    return jsonify({"mensaje": "password cambiado"}),200
+    user = User.query.filter_by(email=email).first()  
+    if user:
+        return jsonify({"mensaje": user.password}),200
+    return  jsonify({"mensaje": "usuario no existe"}),200 
 
 
 
--- src/front/js/component/request_password.js
+++ src/front/js/component/request_password.js
@@ -1,47 +1,86 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useContext } from "react";
+import emailjs from "@emailjs/browser";
+import { Context } from "/workspace/ProyectoFinalOficios/src/front/js/store/appContext.js";
 
-export const RequestPass = () => {
+export const RequestPass  = () => {
+  const { store, actions } = useContext(Context);
   const [email, setEmail] = useState("email");
+  const correo=store.email
+  const form = useRef();
 
-  const handleInputChange = (event) => {
-    setEmail({
-      ...datos,
-      [event.target.email]: event.target.value,
-    });
-  };
+  const sendEmail = (e) => {
+    e.preventDefault();
 
-  const enviarDatos = (event) => {
-    event.preventDefault();
+   
+     var user_pass ="mensaje"
+
+
+     var myHeaders = new Headers();
+     myHeaders.append("Content-Type", "application/json");
+
+    var requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    };
+
+    fetch(
+      "https://3001-juanjoserus-proyectofin-z2ja69dm9cp.ws-eu83.gitpod.io/api/request_password",
+      requestOptions
+    )
+      .then((response) => response.text())
+      .then((result) => {user_pass = result.mensaje}),
+       console.log(result)
+      .catch((error) => console.log("error", error));
+    emailjs
+      .sendForm(
+        "service_ru0grmi",
+        "template_i02czvn",
+        form.current,
+        "_axOPzpSoTd8XH4fF"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
   };
 
   return (
     <div className="container-fluid center">
       <div className="row">
-        <form>
+        <form ref={form} onSubmit={sendEmail}>
           <div className="form-group">
-            <label for="user_email">Recupera tu contrasena</label>
+            <label for="user_email">Recupera tu contraseña</label>
             <input
               type="email"
               class="form-control"
               placeholder="Enter email"
               id="user_email"
-              onChange={handleInputChange}
+              name="user_email"
             ></input>
             <small className="form-text text-muted">
               Ingrese su email registrado
             </small>
+
           </div>
-          <button type="submit" class="btn btn-primary" onSubmit={enviarDatos}>
+          <button
+            type="submit"
+            valu="send"
+            class="btn btn-primary"
+            onSubmit={sendEmail}
+          >
             Enviar
+            <input type="hidden" id="user_pass" name="user_pass"  value= "user_pass"></input> 
           </button>
         </form>
       </div>
-      <script
-        type="text/javascript"
-        src="https://cdn.jsdelivr.net/npm/@emailjs/browser@3/dist/email.min.js"
-      ></script>
-
-      <script type="text/javascript">emailjs.init('kfaW5RF3PL38a9QEP')</script>
     </div>
   );
 };
+
+
